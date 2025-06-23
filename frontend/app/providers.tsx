49c8fb6c88a162772f8791a108345cb25cb571a4
NguyenDucHoangNam <namnguyenduchoang@gmail.com>
2025-06-24 01:42:09 +0700

    feat: fix landing page

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -5,7 +5,7 @@ import { AuthProvider } from "./context/AuthContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="white">
       <AuthProvider>
         {children}
       </AuthProvider>

a1823e9c96a2f398e5751199b0e55ee3dc3e34b8
Rodneynee <domanhhung07082004@gmail.com>
2025-07-01 20:28:38 +0700

    Add Google Tag Manager

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,14 @@
 "use client";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'; import Footer from "@/components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
+import React from "react";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
-import { showSuccess } from '@/utils/toastService';
+import { showSuccess } from "@/utils/toastService";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,14 +19,36 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-      <head />
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M4GVGLSH');
+            `,
+          }}
+        />
+      </head>
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M4GVGLSH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Providers>
           <Header />
           <>
             <ToastContainer
-              position="top-right"        // 👈 Hiển thị góc phải trên
-              autoClose={5000}            // Tự tắt sau 3s
+              aria-label=""
+              position="top-right" // 👈 Hiển thị góc phải trên
+              autoClose={5000} // Tự tắt sau 3s
               hideProgressBar={false}
               newestOnTop={false}
               closeOnClick
@@ -32,8 +56,9 @@ export default function RootLayout({
               pauseOnFocusLoss
               draggable
               pauseOnHover
-              theme="light"              // Có thể đổi sang 'dark' nếu cần
-            />            {children}
+              theme="light" // Có thể đổi sang 'dark' nếu cần
+            />{" "}
+            {children}
           </>
           <Footer />
           <ScrollToTop />

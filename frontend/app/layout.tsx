4bd15ca03af012c58425afe6ee6583ee65cfe5db
NguyenDucHoangNam <namnguyenduchoang@gmail.com>
2025-07-03 06:37:51 +0700

    add Google Tag Manager

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,6 +7,7 @@ import { Inter } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 import { showSuccess } from '@/utils/toastService';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,27 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-      <head />
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WQWLDR6Q');
+          `}
+        </Script>
+      </head>
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WQWLDR6Q"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         <Providers>
           <Header />
           <>
